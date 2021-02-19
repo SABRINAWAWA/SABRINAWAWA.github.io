bc070e2abeeefca493ead714199ebbaa382914d4
Huiying-Sabrina <53490593+SABRINAWAWA@users.noreply.github.com>
2021-02-19 14:01:20 -0800

    upload travel to china website
    upload travel to china website from the local folder

---
+++ Travel_to_China_Website/script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+const colors = ["#220901", "#621708", "#941b0c", "#bc3908", "#ffcf99"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
+Array.from(document.querySelectorAll(".navigation-button")).forEach(item => {
+  item.onclick = () => {
+    item.parentElement.parentElement.classList.toggle("change");
+  };
+});
